9b3f1a82a9835d1d43c88a9d6d31196c8a46c0db
wlnoor <fauzannoor98@gmail.com>
2018-09-25 23:51:27 +0700

    listView with data form json

--- app/src/main/java/com/example/fauzan/android_modules_example/ListviewActivity.java
+++ app/src/main/java/com/example/fauzan/android_modules_example/ListviewActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class ListviewActivity extends AppCompatActivity {
-
-    private String[] universitas = {"Institute Tekhnologi Bandung","Universitas Komputer","Universitas Pendidikan Indonesia","Universitas Gadjah Mada","Universitas Maranatha","Universitas Khatolik Parahyangan","Universitas Bina Sarana Indonesia","Universitas Siliwangin","Universitas Pasundan"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +26,25 @@ public class ListviewActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        /*CustomAdapter customAdapter = new CustomAdapter(this, universitas);
-        listView.setAdapter(customAdapter);*/
+        /*
+        * Untuk membuat listView kita gunakan ArrayList, fungsinya untuk menampung list kedalam ListArray
+        * Setelah membuat object ArrayList, kita gunakan method ``add`` untuk mengisi arrayList yang kita buat
+        * Kita buat customeAdapter fungsinya untuk menampilkan setiap list yang kita buat
+        * */
+        //ArrayList<Universitas> customeArrayList = new ArrayList<>();
+        //customeArrayList.add(new Universitas("Institute Tekhnologi Bandung"));
+        //customeArrayList.add(new Universitas("Institute Pertanian Bogor"));
+        //customeArrayList.add(new Universitas("Universitas Komputer"));
+        //customeArrayList.add(new Universitas("Universitas Pendidikan Indonesia"));
+        //customeArrayList.add(new Universitas("Universitas Padjajaran"));
+        //customeArrayList.add(new Universitas("Universitas BSI Bandung"));
+        //customeArrayList.add(new Universitas("Universitas Pasundan"));
+        //customeArrayList.add(new Universitas("Universitas Maranatha"));
+        //customeArrayList.add(new Universitas("Universitas Gadjah Mada"));
+        //customeArrayList.add(new Universitas("Universitas Indonesia"));
+        //customeArrayList.add(new Universitas("Universitas Parahyangan"));
 
-        ArrayList<Universitas> customeArrayList = new ArrayList<Universitas>();
-        customeArrayList.add(new Universitas("Institute Tekhnologi Bandung"));
-        customeArrayList.add(new Universitas("Universitas Komputer"));
-
-        Log.i("arraList", "" + customeArrayList.size());
+        ArrayList<Universitas> customeArrayList = ExtractJson.extractJsonKampus();
 
         CustomAdapter customAdapter = new CustomAdapter(this,customeArrayList);
         ListView listView = (ListView) findViewById(R.id.listview_activty);
